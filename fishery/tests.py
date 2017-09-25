6cc15d43da6dbc5f0f3e83b6a0bc68120467770d
LAPTOP-7KFM2KSP\User <594395096@qq.com>
2017-09-25 10:26:27 +0800

    add three cases to the bots in fishery app

--- fishery/tests.py
+++ fishery/tests.py
@@ -1,4 +1,6 @@
 from otree.api import Currency as c, currency_range, SubmissionMustFail
+from otree.constants_internal import timeout_happened
+
 from . import views
 from ._builtin import Bot
 from .models import Constants
@@ -7,6 +9,7 @@ from random import randint
 
 
 class PlayerBot(Bot):
+    cases = ['greedy', 'lazy', 'normal']
 
     @property
     def play_round(self):
@@ -14,7 +17,16 @@ class PlayerBot(Bot):
         # do not submit anything, send error
         yield SubmissionMustFail(views.StudentCatch)
 
-        yield (views.StudentCatch, {'num_fish_caught_this_year': randint(0, 2)})
+        fish = {
+            'greedy': 2,
+            'lazy': 0,
+            'normal': randint(0, 2),
+        }[self.case]
+
+        # timeout happened?
+        # yield (views.StudentCatch, {'num_fish_caught_this_year': fish}, timeout_happened)
+
+        yield (views.StudentCatch, {'num_fish_caught_this_year': fish})
         yield (views.OutOfFishResult)
         yield (views.StudentFinalResult)
 
